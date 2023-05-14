913f2bacfda3644fa371e43f609c92e3d4dfe060
Samarth2001 <samarthsr2001@gmail.com>
2023-05-14 23:55:45 +0530

    Frontend

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 
@@ -8,6 +9,9 @@ const app = express();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
+
+// static
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/openai", require("./routes/openaiRoutes"));
 
