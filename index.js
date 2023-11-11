a5f38be23f1dca3dbcbda41d32c7b8aecb6c29e1
Samarth2001 <samarthsr2001@gmail.com>
2023-11-11 23:38:16 +0530

    update

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/openai", require("./routes/openaiRoutes"));
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  console.log(`Server started on port  ${port}`);
 });
